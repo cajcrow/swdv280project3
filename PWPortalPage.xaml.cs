e0f7f2bac0bc4bd291e87f4a1b9b1753f1e53fcb
Josh Shockey <43383790+joshshockey@users.noreply.github.com>
2019-04-17 09:38:45 -0600

    Updated PWPortalPages, added Forms Folder for documents, Images

---
+++ PWPortalPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CWIApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PWPortalPage : ContentPage
+    {
+
+        public PWPortalPage()
+        {
+            InitializeComponent();
+
+            Title = "SWDV Forms and Documents";
+
+        }
+
+        public void OnLabelOverviewClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.CourseOverview());
+        }
+
+        public void OnLabelInternshipClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.InternshipForm());
+        }
+
+        public void OnLabelInternAFClicked(object sender, EventArgs args)
+        {
+           
+            this.Navigation.PushAsync(new Forms.InternAF());
+        }
+
+        public void OnLabelInternTSClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.InternTS());
+        }
+    }
+}
