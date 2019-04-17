cf031d3b296ab4183715028ffe32b4a54d8230d2
Josh Shockey <43383790+joshshockey@users.noreply.github.com>
2019-04-17 10:38:46 -0600

    update click event links

--- CWIApp/CWIApp/PWPortalPage.xaml.cs
+++ CWIApp/CWIApp/PWPortalPage.xaml.cs
@@ -1,39 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CWIApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PWPortalPage : ContentPage
-	{
-		public PWPortalPage ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PWPortalPage : ContentPage
+    {
 
-            BackgroundColor = Color.Red;
-            Content = new Frame
-            {
-                OutlineColor = Color.Black,
-                BackgroundColor = Color.White,
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                Content = new Label
-                {
-                    Text = "This is the Paperwork Forms Portal!",
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                    FontAttributes = FontAttributes.Italic,
-                    TextColor = Color.Red
-                }
-            };
+        public PWPortalPage()
+        {
+            InitializeComponent();
+
+            Title = "SWDV Forms and Documents";
 
         }
 
+        public void OnLabelOverviewClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.CourseOverview());
+        }
 
+        public void OnLabelInternshipClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.InternshipForm());
+        }
+
+        public void OnLabelInternAFClicked(object sender, EventArgs args)
+        {
+           
+            this.Navigation.PushAsync(new Forms.InternAF());
+        }
+
+        public void OnLabelInternTSClicked(object sender, EventArgs args)
+        {
+            
+            this.Navigation.PushAsync(new Forms.InternTS());
+        }
     }
 }
