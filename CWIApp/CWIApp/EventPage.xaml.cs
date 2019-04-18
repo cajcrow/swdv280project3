84df1995a530bfd30bdc441a37b78487afd87ff5
Josh Shockey <43383790+joshshockey@users.noreply.github.com>
2019-04-18 09:43:41 -0600

    updated code behind

--- CWIApp/CWIApp/EventPage.xaml.cs
+++ CWIApp/CWIApp/EventPage.xaml.cs
@@ -1,20 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CWIApp
+namespace Events
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class EventPage : ContentPage
+	public partial class EventsPage : ContentPage
 	{
-		public EventPage ()
+		public EventsPage ()
 		{
 			InitializeComponent ();
 		}
-	}
-}
+
+
+        void OnImageTapped(object sender, EventArgs args)
+        {
+            TappedEventArgs tap = (TappedEventArgs) args;
+
+            webView.Source = tap.Parameter.ToString();
+            overlay.IsVisible = true;
+            webView.IsVisible = true;
+        }
+
+        void OnGoBackClicked(object sender, EventArgs args)
+        {
+            overlay.IsVisible = false;
+            webView.GoBack();    
+        }
+
+        //void OnGoForwardClicked(object sender, EventArgs args)
+        //{
+        //    overlay.IsVisible = false;
+        //    webView.GoForward();
+        //}
+
+    }//end class
+}//end namespace
